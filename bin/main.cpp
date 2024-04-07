7025dcc5fcb581a2d1a4344bbfe9ca34c82a9c06
NizamutdinovEmir <144460470+NizamutdinovEmir@users.noreply.github.com>
2024-04-07 14:31:28 +0300

    Add files via upload

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,1 +1,24 @@
+#include "lib/parsing.h"
+#include "lib/hamming.h"
+#include "lib/archive.h"
 
+int main(int argc, char** argv) {
+  Options options;
+  options.Parse(argc, argv);
+  Archiver archiver;
+  if (options.operation == create) {
+    archiver.Create(options.arch_name, options.file_names, options.control_bits_count);
+  } else if (options.operation == list) {
+    archiver.List(options.arch_name);
+  } else if (options.operation == extract) {
+    archiver.Extract(options.arch_name, options.file_names);
+  } else if (options.operation == append) {
+    archiver.Append(options.arch_name, options.file_names, options.control_bits_count);
+  } else if (options.operation == del) {
+    archiver.Delete(options.arch_name, options.file_names);
+  } else if (options.operation == concatenate) {
+    archiver.Concatenate(options.arch_name, options.file_names);
+  }
+
+  return 0;
+}
